070e2ddfdc666db9d0a49ea434a5d1de71804c78
BinaryBitBytes <117mwc117@gmail.com>
2024-03-20 15:14:33 -0400

    continued psuedocoding the project

---
+++ main.lua
@@ -0,0 +1,30 @@
+
+-- Draw the Screen
+
+-- 2D Screen
+-- -->[Players]
+
+-- -->[Paddle]
+
+-- -->[Ball]
+
+-- -- -->[Actions]
+
+-- -->[Bounds]
+
+-- -->[Walls]
+
+-- -- -- -- Coordinates
+
+-- -- Collision Detection
+
+-- -- -- Physics
+
+-- -- -- -- Time Constants
+
+-- -- -- -- Distance / Time 
+
+-- -- -- -- Events
+
+-- -- -- -- Scoring
+
