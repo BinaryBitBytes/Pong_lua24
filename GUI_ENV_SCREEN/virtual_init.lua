03ee634e5f4fa88f55a10361c8401796d864dade
BinaryBitBytes <117mwc117@gmail.com>
2024-03-22 20:46:14 -0400

    resolved in .git on the .lock by deleting .lock so i can commit changes

--- GUI_ENV_SCREEN/virtual_init.lua
+++
@@ -1,9 +0,0 @@
-function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
-
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizeable = false,
-        vsync = true
-    })
-end
