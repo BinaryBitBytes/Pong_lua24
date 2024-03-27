f4f4602b2888c4d9e995f2f787b843a0a3d8519a
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2024-03-27 11:50:33 -0400

    resolving readme

--- gui.lua
+++
@@ -1,9 +0,0 @@
--- [--!Call Each Frane After Updating Drawing]
-function love.draw()
-    love.graphics.printf(
-        'Hello, Welcome to Pong!',
-        0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
-        'center')
-end
