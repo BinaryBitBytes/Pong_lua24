cffdbf8563489173ed67acae4eaf1f249c9e86ac
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2024-03-30 16:34:39 -0400

    added to push.lua

--- Methods/push.lua
+++ Methods/push.lua
@@ -94,4 +94,10 @@ function push:getCanvasTable(name)
         end
     end
 end
-
+function push:setShader(name, shader)
+    if not shaderthen
+    self:getCannvasTable("render").shader = name
+else
+    se;f:getCanvasTable(name).shader = shader
+end
+end
